11b26a37f06b00a47b898febcc08bab2448ee16b
Marco-LimaBean <Marco@LimaBean.co.za>
2017-09-07 11:28:09 +0200

    v0.3.5 Cleaning up - Added missing git files

--- js/app.js
+++ js/app.js
@@ -1,2 +1,4 @@
 $(document).foundation();
+
+//to reload the page on re-size, fixes the menu system alignment --- NOTE FORM DATA CLEARED IF LATER ADDED
 $(window).on('resize',function(){location.reload();});
